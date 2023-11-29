dc0893f93fe8bbc5bd8d75171ebc949be44eb2cc
hribeiro <henrique.ribeiro@intesys.it>
2023-11-29 20:04:49 +0100

    Initialize React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+
+export function App() {
+  /**
+   * This is not HTML, this is JSX!
+   */
+  return (
+    <div className="container">
+      <input placeholder="username" />
+    </div>
+  );
+}
