7e641633aad64a7939ae2415bd6002f3176e8eba
hribeiro <henrique.ribeiro@intesys.it>
2023-12-06 19:12:58 +0100

    This is an initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./app.css";
 
-export function App() {
+export function App({ title }: { title: string }) {
   /**
    * This is not HTML, this is JSX!
    */
+
   return (
     <div className="container">
+      <h1>{title}</h1>
       <input placeholder="username" />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App.jsx";
+import { App } from "./App";
 
 const appElement = document.getElementById("app");
 
 const root = ReactDOM.createRoot(appElement);
 
-root.render(<App />);
+root.render(<App title={"This should work just fine"} />);
